79a905494e69c88e90960e90e544130714edfa70
Czerny-F <dummy00.cz@gmail.com>
2018-10-01 17:07:14 +0900

    Delete a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -30,4 +30,11 @@ export class HeroesComponent implements OnInit {
         // this.getHeroes();
       });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe(
+      // () => this.getHeroes()
+    );
+  }
 }
